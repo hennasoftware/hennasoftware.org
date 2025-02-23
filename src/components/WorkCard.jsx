27e8c98eb12e92157cb42be9f4a59300ad290e51
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-02-23 17:42:48 -0300

    feat: add HowWeWork section and components

---
+++ src/components/WorkCard.jsx
@@ -0,0 +1,13 @@
+function WorkCard(props) {
+  return (
+    <>
+      <div
+        className={'bg-gradient-to-t from-slate-950 to-slate-900 border-2 border-slate-800 to-slate-700 w-full p-6 rounded-xl text-left flex flex-col gap-4 md:max-w-sm'}>
+        <h4 className={'text-xl'}><i className={`${props.icon} text-2xl`}></i> {props.title}</h4>
+        <p className={'text-slate-500 text-base'}>{props.description}</p>
+      </div>
+    </>
+  );
+}
+
+export default WorkCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import Header from "../components/Header.jsx";
-import Hero from "../components/Hero.jsx";
+import Header from '../components/Header.jsx';
+import Hero from '../components/Hero.jsx';
 import OurStory from '../components/OurStory.jsx';
+import HowWeWork from '../components/HowWeWork.jsx';
 
 function Home() {
   return (
     <>
-      <Header/>
-      <Hero/>
+      <Header />
+      <Hero />
       <OurStory />
+      <HowWeWork />
     </>
-  )
+  );
 }
 
 export default Home;
