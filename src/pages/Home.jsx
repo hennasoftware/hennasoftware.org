da8db34e0aa29ef40b1f1bb2dd89c0ffaa792b7d
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-02-23 17:42:27 -0300

    feat: add OurStory section and components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx";
 import Hero from "../components/Hero.jsx";
+import OurStory from '../components/OurStory.jsx';
 
 function Home() {
   return (
     <>
       <Header/>
       <Hero/>
+      <OurStory />
     </>
   )
 }
