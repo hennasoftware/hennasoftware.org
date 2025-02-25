7ec0591cae26e1810892fa1057aaac1d3cee8d5c
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-02-25 13:29:11 -0300

    refactor: minor codebase updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,10 @@ function Nav() {
     <>
       <div className={'hidden md:flex'}>
         <ul className={'flex gap-6 text-lg'}>
-          <li><a href="/" className={'transition-all hover:text-blue-500'}>Home</a></li>
-          <li><a href="#about" className={'transition-all hover:text-blue-500'}>About</a></li>
-          <li><a href="#projects" className={'transition-all hover:text-blue-500'}>Projects</a></li>
-          <li><a href="#contact" className={'transition-all hover:text-blue-500'}>Contact</a></li>
+          <li><a href={'/'} className={'transition-all hover:text-blue-500'}>Home</a></li>
+          <li><a href={'#about'} className={'transition-all hover:text-blue-500'}>About</a></li>
+          <li><a href={'#projects'} className={'transition-all hover:text-blue-500'}>Projects</a></li>
+          <li><a href={'#join'} className={'transition-all hover:text-blue-500'}>Contact</a></li>
         </ul>
       </div>
     </>
